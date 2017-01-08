1c141ad220f9a72ea8f0c2ded48df765e5dcf1a7
Jeremy Trunk <trunk.jeremy@gmail.com>
2017-01-07 23:54:45 -0500

    Trying layouts - BorderPane, GridPane

--- my-app/src/main/java/com/theupquark/app/JavaFXWindow.java
+++ my-app/src/main/java/com/theupquark/app/JavaFXWindow.java
@@ -1,8 +1,13 @@
 package com.theupquark.app;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -11,7 +16,7 @@ public class JavaFXWindow extends Application {
   public void start(Stage primaryStage) {
     
     Button btOK = new Button("OK");
-    Scene scene = new Scene(btOK);
+    Scene scene = new Scene(this.getPane(), 600, 200);
     primaryStage.setTitle("JavaFXWindow");
     primaryStage.setScene(scene);
     primaryStage.show();
@@ -20,9 +25,32 @@ public class JavaFXWindow extends Application {
   }
 
   /**
-   * Not used
+   * Main
    */
   public static void main(String[] args) {
     Application.launch(args);
   } 
+
+  /**
+   * Sets BorderPane
+   *
+   * @returns borderPane
+   */
+  private BorderPane getPane() {
+    BorderPane bPane = new BorderPane();
+
+    //TOP
+    Text title = new Text(100, 50, "Window Text");
+    GridPane topPane = new GridPane();
+    topPane.add(title, 0, 0);
+    topPane.setAlignment(Pos.CENTER);
+    bPane.setTop(topPane);
+
+    //CENTER
+    Pane screen = new Pane();
+    screen.setStyle("-fx-background-color: black");
+    screen.setPrefSize(400, 100);
+    bPane.setCenter(screen);
+    return bPane;
+  }
 } 
